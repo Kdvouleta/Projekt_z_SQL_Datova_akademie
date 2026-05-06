6c58e819f9ecae02bb42e2b44a06f1a4a9d9531b
Kdvouleta <katerina.dvouleta@szif.gov.cz>
2026-05-06 20:35:34 +0200

    Update Dvouleta_Katerina_sql projekt_otazka 3.sql

--- Dvouleta_Katerina_sql projekt_otazka 3.sql
+++ Dvouleta_Katerina_sql projekt_otazka 3.sql
@@ -4,48 +4,48 @@
 WITH prices_unique AS (
 SELECT DISTINCT
 year,
-category_code,
-category_name,
-avg_price
+    category_code,
+    category_name,
+    avg_price
 FROM t_katerina_dvouleta_project_sql_primary_final
 ),
-price_growth AS (
+    price_growth AS (
 SELECT
 year,
-category_code,
-category_name,
-avg_price,
-LAG(avg_price) OVER (
-PARTITION BY category_code
+    category_code,
+    category_name,
+    avg_price,
+    LAG(avg_price) OVER (
+    PARTITION BY category_code
 ORDER BY year
-) AS prev_year_price
+    ) AS prev_year_price
 FROM prices_unique
 ),
-growth_calculated AS (
+    growth_calculated AS (
 SELECT
 year,
-category_code,
-category_name,
-avg_price,
-prev_year_price,
-ROUND(
-((avg_price - prev_year_price) / prev_year_price * 100)::numeric,
-2
+    category_code,
+    category_name,
+    avg_price,
+    prev_year_price,
+    ROUND(
+    ((avg_price - prev_year_price) / prev_year_price * 100)::numeric,
+    2
 ) AS pct_growth
 FROM price_growth
 WHERE prev_year_price IS NOT NULL
 )
 SELECT
-category_code,
-category_name,
-ROUND(AVG(pct_growth), 2) AS avg_pct_growth
+    category_code,
+    category_name,
+    ROUND(AVG(pct_growth), 2) AS avg_pct_growth
 FROM growth_calculated
 GROUP BY
-category_code,
-category_name
+    category_code,
+    category_name
 ORDER BY avg_pct_growth;
 
--- odpověď na otázku č. 3 - Nejpomaleji zdražovalo zboží Cukr krystalový (118101), u které vyšel průměrný meziroční procentní růst -1,92%. Jeho cena v průměru meziročně klesala
+-- odpověď na otázku č. 3 - Nejpomaleji zdražovalo zboží Cukr krystalový (118101), u které vyšel průměrný meziroční procentní růst -1,92 %. Jeho cena v průměru meziročně klesala
 -- Mezi další velmi pomalu rostoucí nebo klesající položky patřila rajská jablka červená kulatá.
 
 
