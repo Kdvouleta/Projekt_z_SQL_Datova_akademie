d162969ee06755fcf4c6b34990a0a8d14a2f42cc
Kdvouleta <katerina.dvouleta@szif.gov.cz>
2026-05-06 19:39:42 +0200

    Update Dvouleta_Katerina_sql projekt.sql

--- Dvouleta_Katerina_sql projekt.sql
+++ Dvouleta_Katerina_sql projekt.sql
@@ -1,19 +1,8 @@
 
-SELECT * FROM czechia_payroll LIMIT 10;
-SELECT * FROM czechia_payroll_calculation;
-SELECT * FROM czechia_payroll_industry_branch LIMIT 10;
-SELECT * FROM czechia_payroll_unit LIMIT 10;
-SELECT * FROM czechia_payroll_value_type LIMIT 10;
-SELECT * FROM czechia_price LIMIT 10;
-SELECT * FROM czechia_price_category LIMIT 10;
-SELECT * FROM countries;
-SELECT * FROM economies LIMIT 10;
-
 -- prohlédla jsem si tabulky a zjistila jsem jaké mám roky, mzdy po letech či kvartálech, 
 --jaké kódy značí průměrnou mzdu, jak jsou vedeny ceny potravin
 
-
--- dále jsme zjistila společné období mezi cenami a mzdami
+-- dále jsem zjistila společné období mezi cenami a mzdami
 WITH payroll_years AS (
     SELECT DISTINCT payroll_year AS year
     FROM czechia_payroll
@@ -80,61 +69,61 @@ ORDER BY year, category_name;
 
 WITH payroll_yearly AS (
 SELECT
-cp.payroll_year AS year,
-cp.industry_branch_code,
-ib.name AS industry_name,
-ROUND(AVG(cp.value), 2) AS avg_wage
+    cp.payroll_year AS year,
+    cp.industry_branch_code,
+    ib.name AS industry_name,
+    ROUND(AVG(cp.value), 2) AS avg_wage
 FROM czechia_payroll cp
 JOIN czechia_payroll_industry_branch ib
-ON cp.industry_branch_code = ib.code
+    ON cp.industry_branch_code = ib.code
 WHERE cp.value IS NOT NULL
-AND cp.value_type_code = 5958
-AND cp.unit_code = 200
-AND cp.calculation_code = 200
-AND cp.payroll_year BETWEEN 2006 AND 2018
+    AND cp.value_type_code = 5958
+    AND cp.unit_code = 200
+    AND cp.calculation_code = 200
+    AND cp.payroll_year BETWEEN 2006 AND 2018
 GROUP BY
-cp.payroll_year,
-cp.industry_branch_code,
-ib.name
+    cp.payroll_year,
+    cp.industry_branch_code,
+    ib.name
 ),
 price_yearly AS (
 SELECT
-EXTRACT(YEAR FROM cp.date_from) AS year,
-cp.category_code,
-cpc.name AS category_name,
-cpc.price_value,
-cpc.price_unit,
-ROUND(AVG(cp.value)::numeric, 2) AS avg_price
+    EXTRACT(YEAR FROM cp.date_from) AS year,
+    cp.category_code,
+    cpc.name AS category_name,
+    cpc.price_value,
+    cpc.price_unit,
+    ROUND(AVG(cp.value)::numeric, 2) AS avg_price
 FROM czechia_price cp
 JOIN czechia_price_category cpc
-ON cp.category_code = cpc.code
+    ON cp.category_code = cpc.code
 WHERE cp.value IS NOT NULL
-AND cp.region_code IS NULL
-AND EXTRACT(YEAR FROM cp.date_from) BETWEEN 2006 AND 2018
+    AND cp.region_code IS NULL
+    AND EXTRACT(YEAR FROM cp.date_from) BETWEEN 2006 AND 2018
 GROUP BY
-EXTRACT(YEAR FROM cp.date_from),
-cp.category_code,
-cpc.name,
-cpc.price_value,
-cpc.price_unit
+    EXTRACT(YEAR FROM cp.date_from),
+    cp.category_code,
+    cpc.name,
+    cpc.price_value,
+    cpc.price_unit
 )
 SELECT
-p.year,
-p.industry_branch_code,
-p.industry_name,
-p.avg_wage,
-c.category_code,
-c.category_name,
-c.price_value,
-c.price_unit,
-c.avg_price
+    p.year,
+    p.industry_branch_code,
+    p.industry_name,
+    p.avg_wage,
+    c.category_code,
+    c.category_name,
+    c.price_value,
+    c.price_unit,
+    c.avg_price
 FROM payroll_yearly p
 JOIN price_yearly c
-ON p.year = c.year
+    ON p.year = c.year
 ORDER BY
-p.year,
-p.industry_name,
-c.category_name;
+    p.year,
+    p.industry_name,
+    c.category_name;
 
 -- vytvořila jsem tabulku primary_final dle zadání
 
@@ -143,79 +132,73 @@ DROP TABLE IF EXISTS t_katerina_dvouleta_project_sql_primary_final;
 CREATE TABLE t_katerina_dvouleta_project_sql_primary_final AS
 WITH payroll_yearly AS (
 SELECT
-cp.payroll_year AS year,
-cp.industry_branch_code,
-ib.name AS industry_name,
-ROUND(AVG(cp.value), 2) AS avg_wage
+    cp.payroll_year AS year,
+    cp.industry_branch_code,
+    ib.name AS industry_name,
+    ROUND(AVG(cp.value), 2) AS avg_wage
 FROM czechia_payroll cp
 JOIN czechia_payroll_industry_branch ib
-ON cp.industry_branch_code = ib.code
+    ON cp.industry_branch_code = ib.code
 WHERE cp.value IS NOT NULL
-AND cp.value_type_code = 5958
-AND cp.unit_code = 200
-AND cp.calculation_code = 200
-AND cp.payroll_year BETWEEN 2006 AND 2018
+    AND cp.value_type_code = 5958
+    AND cp.unit_code = 200
+    AND cp.calculation_code = 200
+    AND cp.payroll_year BETWEEN 2006 AND 2018
 GROUP BY
-cp.payroll_year,
-cp.industry_branch_code,
-ib.name
+    cp.payroll_year,
+    cp.industry_branch_code,
+    ib.name
 ),
 price_yearly AS (
 SELECT
-EXTRACT(YEAR FROM cp.date_from) AS year,
-cp.category_code,
-cpc.name AS category_name,
-cpc.price_value,
-cpc.price_unit,
-ROUND(AVG(cp.value)::numeric, 2) AS avg_price
+    EXTRACT(YEAR FROM cp.date_from) AS year,
+    cp.category_code,
+    cpc.name AS category_name,
+    cpc.price_value,
+    cpc.price_unit,
+    ROUND(AVG(cp.value)::numeric, 2) AS avg_price
 FROM czechia_price cp
 JOIN czechia_price_category cpc
-ON cp.category_code = cpc.code
+    ON cp.category_code = cpc.code
 WHERE cp.value IS NOT NULL
-AND cp.region_code IS NULL
-AND EXTRACT(YEAR FROM cp.date_from) BETWEEN 2006 AND 2018
+    AND cp.region_code IS NULL
+    AND EXTRACT(YEAR FROM cp.date_from) BETWEEN 2006 AND 2018
 GROUP BY
-EXTRACT(YEAR FROM cp.date_from),
-cp.category_code,
-cpc.name,
-cpc.price_value,
-cpc.price_unit
+    EXTRACT(YEAR FROM cp.date_from),
+    cp.category_code,
+    cpc.name,
+    cpc.price_value,
+    cpc.price_unit
 )
 SELECT
-p.year,
-p.industry_branch_code,
-p.industry_name,
-p.avg_wage,
-c.category_code,
-c.category_name,
-c.price_value,
-c.price_unit,
-c.avg_price
+    p.year,
+    p.industry_branch_code,
+    p.industry_name,
+    p.avg_wage,
+    c.category_code,
+    c.category_name,
+    c.price_value,
+    c.price_unit,
+    c.avg_price
 FROM payroll_yearly p
 JOIN price_yearly c
-ON p.year = c.year;
-
-SELECT *
-FROM t_katerina_dvouleta_project_sql_primary_final
-LIMIT 20;
-
-SELECT COUNT(*)
-FROM t_katerina_dvouleta_project_sql_primary_final;
+    ON p.year = c.year;
 
 -- zkontrolovala jsem data pro secondary_final, zjistila jsem, že se to bude týkat roků 2006 - 2018
+-- cíl získat pro evropské státy a roky 2006 - 2018 tyto sloupce: country, year, gdp, gini, population
 
 SELECT
-c.country,
-c.continent,
-e.year,
-e.gdp,
-e.gini,
-e.population
+    c.country,
+    c.continent,
+    e.year,
+    e.gdp,
+    e.gini,
+    e.population
 FROM countries c
 JOIN economies e
-ON c.country = e.country
+    ON c.country = e.country
 WHERE c.continent = 'Europe'
-AND e.year BETWEEN 2006 AND 2018
+    AND e.year BETWEEN 2006 AND 2018
 ORDER BY c.country, e.year;
 
 -- vytvorila jsem tabulku secondary_final
@@ -224,21 +207,14 @@ DROP TABLE IF EXISTS t_katerina_dvouleta_project_sql_secondary_final;
 
 CREATE TABLE t_katerina_dvouleta_project_sql_secondary_final AS
 SELECT
-c.country,
-c.continent,
-e.year,
-e.gdp,
-e.gini,
-e.population
+    c.country,
+    c.continent,
+    e.year,
+    e.gdp,
+    e.gini,
+    e.population
 FROM countries c
 JOIN economies e
-ON c.country = e.country
+    ON c.country = e.country
 WHERE c.continent = 'Europe'
-AND e.year BETWEEN 2006 AND 2018;
-
-SELECT *
-FROM t_katerina_dvouleta_project_sql_secondary_final
-LIMIT 20;
-
-
-
+    AND e.year BETWEEN 2006 AND 2018;
